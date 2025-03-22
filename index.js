e3beabdfdba02992cfb7e362b47f3394edf06db5
tinph010803 <tinphan924@gmail.com>
2025-03-22 21:17:33 +0700

    test jenkins

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/contact', function (req, res) {
+app.get('/tin', function (req, res) {
     res.send('Hello World');
 });
 app.listen(3000, function () {
