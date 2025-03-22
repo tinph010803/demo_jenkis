aab61f19c8a69b37d5082131ca61d219427b3016
tinph010803 <tinphan924@gmail.com>
2025-03-22 21:10:40 +0700

    test jenkins

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/contact', function (req, res) {
-    res.send('Hello node2123');
+    res.send('Hello World');
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
